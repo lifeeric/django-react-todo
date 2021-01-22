9b8ae55d082ed60e1285716adf8b1acb3cedaca6
EricGit <ericrothstack@gmail.com>
2021-01-22 08:23:43 +0500

    comment static_URL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # we whitelist localhost:3000 because that's where
 # frontend will be served
