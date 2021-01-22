c292ab176724ce6679b1b4f946d47c3a82dd4467
EricGit <ericrothstack@gmail.com>
2021-01-22 07:31:20 +0500

    boom, v1

---
+++ backend/todo/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
