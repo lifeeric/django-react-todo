98c7ba1baf33cf44d3ef92e510e9121421a097a5
EricGit <ericrothstack@gmail.com>
2021-01-23 12:46:04 +0500

    rm /api/todos

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://dtodo2.herokuapp.com/api/todos",
+  baseURL: "https://dtodo2.herokuapp.com/",
 //   baseURL: "http://localhost:8000",
 });
 
